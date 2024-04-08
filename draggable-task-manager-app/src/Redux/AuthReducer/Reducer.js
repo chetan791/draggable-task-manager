74faac4f2690392e39c8bb47c4a2b338d9f0b44a
chetan791 <chetanchauhan795@gmail.com>
2024-04-08 12:07:38 +0530

    removed development token

--- draggable-task-manager-app/src/Redux/AuthReducer/Reducer.js
+++ draggable-task-manager-app/src/Redux/AuthReducer/Reducer.js
@@ -3,8 +3,7 @@ import { LOGINFAILURE, LOGINSUCCESS, LOGOUT } from "../ActionTypes";
 const InitialState = {
   isAuthenticated: false,
   name: "",
-  token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2NjAyYjdjNmE2MWNjZTE4NDczYzFhNTYiLCJpYXQiOjE3MTI0MzUyNTYsImV4cCI6MTcxNTAyNzI1Nn0.bR9HsRSpee8ru-yBnwIX7q-7bHT_XDnj19VpzGt0G5A",
+  token: "",
   error: "",
 };
 
